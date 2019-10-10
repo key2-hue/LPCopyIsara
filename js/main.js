67a03a62e7038f086fab912d2b42188e2d3311ef
key2-hue <keymaeda3782@gmail.com>
2019-10-10 20:43:15 +0900

    10/10 20:43

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 $(function(){
   $('.flow-js > img, .flow-js > p').on('click' , function(){
     var twoParent = $(this).parent().parent();
-    if(twoParent.find('div:last-child').is(':hidden')){
-      $('.display-main').slideUp(300);
-      $('.display-main').removeClass('display-main');
-      twoParent.find('div:last-child').addClass('display-main').slideDown(300)
-      $(this).parent().find('p').addClass('border');
-    } else {
-      twoParent.find('div:last-child').slideUp(300);
-      // $(this).parent().find('p').removeClass('border');
-    }
+    var width = $(window).width();
+    var slide = 769;
+    if(width < slide ) {
+      if(twoParent.find('div:last-child').is(':hidden')){
+        $('.display-main').slideUp(300);
+        $('.display-main').removeClass('display-main');
+        twoParent.find('div:last-child').addClass('display-main').slideDown(300)
+        $(this).parent().find('p').addClass('border');
+      } else {
+        twoParent.find('div:last-child').slideUp(300);
+        // $(this).parent().find('p').removeClass('border');
+      }
+    } 
+    
   });
   $('.fourteenth-section').not('.flow-js > img, .flow-js > p').on('click', function(){
     $('.display-main').parent().find('div > p:first-of-type').removeClass('border');
